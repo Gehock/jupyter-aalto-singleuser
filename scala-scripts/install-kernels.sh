8655d1c37c76321e9071ec150de751ab987c7b34
Sami Laine <sami.v.laine@aalto.fi>
2024-09-19 13:51:56 +0300

    deps flag

--- scala-scripts/install-kernels.sh
+++ scala-scripts/install-kernels.sh
@@ -17,7 +17,7 @@ for SCALA_FULL_VERSION in ${SCALA_VERSIONS}; do
       -r jitpack \
       -i user -I user:sh.almond:scala-kernel-api_${SCALA_FULL_VERSION}:${ALMOND_VERSION} \
       sh.almond:scala-kernel_${SCALA_FULL_VERSION}:${ALMOND_VERSION} \
-      org.scalameta:munit_3:1.0.2 \
+      --dependencies org.scalameta:munit_3:1.0.2 \
       --default=true --sources \
       -o almond ${EXTRA_ARGS[@]}
   ./almond \
@@ -25,7 +25,6 @@ for SCALA_FULL_VERSION in ${SCALA_VERSIONS}; do
     --log info \
     --metabrowse \
     --id scala${SCALA_MAJOR_VERSION_TRIMMED} \
-    --auto-dependency org.scalameta::munit:1.0.2 \
     --display-name "Scala ${SCALA_MAJOR_VERSION}"
   rm -f almond
 done
